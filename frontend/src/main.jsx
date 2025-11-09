52a260853d32d93c13f356b090c43a68c2110b89
CCeleste6 <championceleste6@gmail.com>
2025-11-09 10:58:27 -0300

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Activities from './pages/Activities';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="activities" element={<Activities />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
